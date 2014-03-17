4c012985a49a4864db0ddee98ceaff852da8e632
Scott Snider <sjsnider@gmail.com>
2014-03-16 23:51:49 -0700

    reject function now works

--- src/underbar.js
+++ src/underbar.js
@@ -85,6 +85,16 @@ var _ = { };
   _.reject = function(collection, test) {
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
+    var isEven = function(num) { return num % 2 === 0; };
+    var isOdd = function(num) { return num % 2 !== 0; };
+    if(test(2)){
+      var array = _.filter(collection, isOdd);
+    }
+    else {
+      var array = _.filter(collection, isEven);
+    }
+    
+    return array;
   };
 
   // Produce a duplicate-free version of the array.
