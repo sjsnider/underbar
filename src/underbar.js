13b386b654e04a7bb01bd46d03240e2fb9f43a95
Scott Snider <sjsnider@gmail.com>
2014-03-17 14:54:21 -0700

    update .reduce so that .contain can operate on objects

--- src/underbar.js
+++ src/underbar.js
@@ -182,9 +182,16 @@ var _ = { };
   //     return total + number;
   //   }, 0); // should be 6
   _.reduce = function(collection, iterator, accumulator) {
-    for (var i=0; i<collection.length; i++){
-      accumulator = iterator(accumulator, collection[i]);
-  }
+    if (Array.isArray(collection)){
+      for (var i=0; i<collection.length; i++){
+        accumulator = iterator(accumulator, collection[i]);
+      }
+    }
+    else if (typeof collection === "object") {
+      for (var i in collection){
+        accumulator = iterator(accumulator, collection[i]);
+      }
+    }
   return accumulator;
   };
 
