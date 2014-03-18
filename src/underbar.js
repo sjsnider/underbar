83716cbefff25092fa5d07d5a71ba0b67f5f2a93
Scott Snider <sjsnider@gmail.com>
2014-03-18 00:58:17 -0700

    .memoize passes

--- src/underbar.js
+++ src/underbar.js
@@ -339,6 +339,11 @@ var _ = { };
   // already computed the result for the given argument and return that value
   // instead if possible.
   _.memoize = function(func) {
+    var memo = {};
+   return function() {
+      var result = func.apply(this, arguments);
+      return result;
+   };
   };
 
   // Delays a function for the given number of milliseconds, and then calls
